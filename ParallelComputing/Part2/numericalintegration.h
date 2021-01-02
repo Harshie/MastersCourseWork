a86b8f1c749a75502571f0f06eaf17ec07ba6b02
Harshad Kumar <eharshad23@gmail.com>
2021-01-02 19:27:11 +0000

    rm

--- ParallelComputing/Part2/numericalintegration.h
+++
@@ -1,3 +0,0 @@
-double trapezoidal(float a, double b, int N);
-float simpson(float a, double b, int N);
-double gaussquadrature(float a, double b);
--- ParallelComputing/Readme.txt
+++
@@ -1,1 +0,0 @@
-This folder holds the programs that were worked on using C language with parallel computing.
